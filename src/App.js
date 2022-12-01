aea87e20591504a9e52b4d1628118f252354a67a
Joseph <afiliateejoseph@gmail.com>
2022-12-01 07:40:44 +0100

    nav completed

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './index.css'
+
+const App = () => {
+  return (
+    <div id='body'>
+      <div id='nav'>
+        <div id='links'>
+          <span id='initials'>J</span>
+          <span id='nav-links'>
+            <span id='name'><b>JOSEPH BOLA-AJAYI</b></span>
+            <span className='nav-main'>CONTACTS</span>
+            <span className='nav-main'>ABOUT</span>
+            <span className='nav-main'>PORTFOLIO</span>
+          </span>
+        </div>
+      </div>
+    </div>
+  )
+}
+export default App;
